3ff55e9c5aceb28d308a0c7dfd6348b7e1fad5a9
Madeuss <mateus1two3@gmail.com>
2023-04-02 05:40:19 -0300

    feat: implement auth login, guard and local strategy

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common'
+import { AuthService } from 'src/auth/auth.service'
+import { LocalAuthGuard } from 'src/auth/guards/local-auth.guard'
 
-@Controller('auth')
-export class AuthController {}
+@Controller()
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(LocalAuthGuard)
+  login() {
+    // return this.authService.login()
+  }
+}
