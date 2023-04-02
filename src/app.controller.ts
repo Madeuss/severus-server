9f78ab44dcbe2e90fb1d625ad031509002e67a85
Madeuss <mateus1two3@gmail.com>
2023-04-02 15:13:37 -0300

    feat: implement current user decorator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common'
+import { AppService } from './app.service'
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator'
+import { User } from 'src/user/entities/user.entity'
 
 @Controller()
 export class AppController {
@@ -7,6 +9,11 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHello()
+  }
+
+  @Get('me')
+  getMe(@CurrentUser() user: User) {
+    return user
   }
 }
