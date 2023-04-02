2303ee407c77915fff7ca4e2d8fc0755fc292477
Madeuss <mateus1two3@gmail.com>
2023-04-02 15:05:05 -0300

    feat: implement jwt validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,6 +7,7 @@ import {
   UseGuards,
 } from '@nestjs/common'
 import { AuthService } from 'src/auth/auth.service'
+import { IsPublic } from 'src/auth/decorators/is-public.decorator'
 import { LocalAuthGuard } from 'src/auth/guards/local-auth.guard'
 import { AuthRequest } from 'src/auth/models/AuthRequest'
 
@@ -14,6 +15,7 @@ import { AuthRequest } from 'src/auth/models/AuthRequest'
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @IsPublic()
   @Post('login')
   @HttpCode(HttpStatus.OK)
   @UseGuards(LocalAuthGuard)
