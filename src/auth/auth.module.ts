dcded1427de85209cefedbb58ca0179f4e1f7fb6
Madeuss <mateus1two3@gmail.com>
2023-04-02 15:41:05 -0300

    feat: created login validation middleware

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common'
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 import { LocalStrategy } from 'src/auth/strategies/local.strategy'
 import { UserModule } from 'src/user/user.module'
 import { JwtModule } from '@nestjs/jwt'
 import { JwtStrategy } from 'src/auth/strategies/jwt.strategy'
+import { LoginValidationMiddleware } from 'src/auth/middleware/login-validation.middleware'
 
 @Module({
   imports: [
@@ -17,4 +18,8 @@ import { JwtStrategy } from 'src/auth/strategies/jwt.strategy'
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy, JwtStrategy],
 })
-export class AuthModule {}
+export class AuthModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoginValidationMiddleware).forRoutes('login')
+  }
+}
